bf4ea67a07748814277d2892289e1f3e5a1301d3
Rich <richjdurley@gmail.com>
2016-05-27 08:36:41 +0100

    fibonnaci-kata-first-commit

---
+++ src/main/java/fibonnaci/FibonnaciSequenceGenerator.java
@@ -0,0 +1,25 @@
+package fibonnaci;
+
+public class FibonnaciSequenceGenerator {
+
+  String fib = "0,1,1,2,3,5";
+
+  public int get(int userInput) {
+    int n = 1;
+    int n_1 = 0;
+    int output = 0;
+
+    if (userInput == 1)
+      return 0;
+    else if (userInput == 2)
+      return 1;
+    else {
+      for (int i = 3; i < userInput + 1; i++) {
+        output = n_1 + n;
+        n_1 = n;
+        n = output;
+      }
+      return output;
+    }
+  }
+}
---
+++ src/test/java/fibonnaci/FibonnaciSequenceShould.java
@@ -0,0 +1,60 @@
+package fibonnaci;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class FibonnaciSequenceShould {
+
+  FibonnaciSequenceGenerator fibonnaciSequenceGenerator = new FibonnaciSequenceGenerator();
+
+  @Test
+  public void return0WhenUserInputIs1() {
+    //given
+    int userInput = 1;
+
+    //when
+    int sequenceNumber = fibonnaciSequenceGenerator.get(userInput);
+
+    //then
+    assertTrue(sequenceNumber == 0);
+  }
+
+  @Test
+  public void return1WhenUserInputIs2() {
+    //given
+    int userInput = 2;
+
+    //when
+    int sequenceNumber = fibonnaciSequenceGenerator.get(userInput);
+
+    //then
+    assertThat("sequence", sequenceNumber, is(1));
+  }
+
+  @Test
+  public void return2WhenUserInputIs4() {
+    //given
+    int userInput = 4;
+
+    //when
+    int sequenceNumber = fibonnaciSequenceGenerator.get(userInput);
+
+    //then
+    assertThat("sequence", sequenceNumber, is(2));
+  }
+
+  @Test
+  public void return8WhenUserInputIs7() {
+    //given
+    int userInput = 7;
+
+    //when
+    int sequenceNumber = fibonnaciSequenceGenerator.get(userInput);
+
+    //then
+    assertThat("sequence", sequenceNumber, is(8));
+  }
+
+}
+
